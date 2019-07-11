148d5e8f68dce9eac13b4e68ba888177a7b7bd85
rynolupkebitcube <52450246+rynolupkebitcube@users.noreply.github.com>
2019-07-11 15:41:19 +0200

    End of Chapter 17

--- DatingApp.API/Migrations/20190711121405_mysqlinitial.Designer.cs
+++ DatingApp.API/Migrations/20190711121405_mysqlinitial.Designer.cs
@@ -9,14 +9,15 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DatingApp.API.Migrations
 {
     [DbContext(typeof(DataContext))]
-    [Migration("20190711065845_AddedLikeEntity")]
-    partial class AddedLikeEntity
+    [Migration("20190711121405_mysqlinitial")]
+    partial class mysqlinitial
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
-                .HasAnnotation("ProductVersion", "2.2.4-servicing-10062");
+                .HasAnnotation("ProductVersion", "2.2.4-servicing-10062")
+                .HasAnnotation("Relational:MaxIdentifierLength", 64);
 
             modelBuilder.Entity("DatingApp.API.Models.Like", b =>
                 {
